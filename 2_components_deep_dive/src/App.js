2817741c0f73667eaf9ffdd464f9a89ecc525a02
Kshitij Kumar <kshitij@Purnimas-MacBook-Pro.local>
2024-06-23 17:00:07 +0530

    Completed Assignment 2: Components Deep Dive

--- 2_components_deep_dive/src/App.js
+++ 2_components_deep_dive/src/App.js
@@ -1,28 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, PureComponent } from 'react';
+import WithLogging from './components/HOC_Logger'
 
-function Book({title, author, year}) {
-  return (<div className='Book'>
-    <h3 className='Book-title'>{title}</h3>
-    <p className='Book-author'>Author: {author}</p>
-    <p className='Book-year'>Year: {year}</p>
-  </div>);
+function BookDetail(props) {
+  return (
+    <>
+      <h3 className='Book-title'>{props.title}</h3>
+      <p className='Book-author'>Author: {props.author}</p>
+      <p className='Book-year'>Year: {props.year}</p>
+    </>
+  );
 }
 
-function BookList() {
-  const books = [
-    <Book {...{ title: 'Book 1', author: 'Author 1', year: 2020 }}/>,
-    <Book {...{ title: 'Book 2', author: 'Author 2', year: 2018 }}/>,
-    <Book {...{ title: 'Book 3', author: 'Author 3', year: 2022 }}/>
-    // Add more books if you'd like
-  ];
+class Book extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {...props}
+  }
 
-  return books.map((book, index) => <li className='Booklist' key={index}>{book}</li>);
+  render() {
+    return (<div className='Book'>
+      <BookDetail {...this.state} />
+    </div>);
+  }
+}
+
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+    const books = [
+      <Book {...{ title: 'Book 1', author: 'Author 1', year: 2020 }}/>,
+      <Book {...{ title: 'Book 2', author: 'Author 2', year: 2018 }}/>,
+      <Book {...{ title: 'Book 3', author: 'Author 3', year: 2022 }}/>
+      // Add more books if you'd like
+    ];
+    this.state = {
+      books: books
+    };
+  }
+
+  render() {
+    return this.state.books.map((book, index) => <li className='Booklist' key={index}>{book}</li>);
+  }
 }
 
 function App() {
+  const BookListWithLogging = WithLogging(BookList)
   return (
-    <BookList />
+    <BookListWithLogging />
   );
 }
 
