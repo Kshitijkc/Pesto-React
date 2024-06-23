c643f70fbd626a8cbc46b5afaedf0144446eb497
Kshitij Kumar <kshitij@Purnimas-MacBook-Pro.local>
2024-06-23 17:19:02 +0530

    Copied 2_components_deep_dive into 3_react_props_and_state

---
+++ 3_react_props_and_state/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component, PureComponent } from 'react';
+import WithLogging from './components/HOC_Logger'
+
+function BookDetail(props) {
+  return (
+    <>
+      <h3 className='Book-title'>{props.title}</h3>
+      <p className='Book-author'>Author: {props.author}</p>
+      <p className='Book-year'>Year: {props.year}</p>
+    </>
+  );
+}
+
+class Book extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {...props}
+  }
+
+  render() {
+    return (<div className='Book'>
+      <BookDetail {...this.state} />
+    </div>);
+  }
+}
+
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+    const books = [
+      <Book {...{ title: 'Book 1', author: 'Author 1', year: 2020 }}/>,
+      <Book {...{ title: 'Book 2', author: 'Author 2', year: 2018 }}/>,
+      <Book {...{ title: 'Book 3', author: 'Author 3', year: 2022 }}/>
+      // Add more books if you'd like
+    ];
+    this.state = {
+      books: books
+    };
+  }
+
+  render() {
+    return this.state.books.map((book, index) => <li className='Booklist' key={index}>{book}</li>);
+  }
+}
+
+function App() {
+  const BookListWithLogging = WithLogging(BookList)
+  return (
+    <BookListWithLogging />
+  );
+}
+
+export default App;
