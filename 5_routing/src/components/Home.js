b49a256b5f3fc0bc94ee35fbe781b8b34cb9d81b
Kshitij Kumar <kshitij@Purnimas-MacBook-Pro.local>
2024-07-21 13:25:22 +0530

    Added surname

--- 5_routing/src/components/Home.js
+++ 5_routing/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Kshitij Kumar</h1>
+      <h1>Kshitij Kumar Choudhary</h1>
       <p>An Explorer</p>
     </div>
   );
